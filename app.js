96e0fa8041186ae9db59f655c15e55da4ca7c85c
Muhammed Nowfal <nowfalmmuhammed@gmail.com>
2025-03-17 17:28:22 +0530

    updates

--- app.js
+++ app.js
@@ -22,32 +22,39 @@ app.use(json());
 let users = [];
 let toDos = [];
 
+function log() {
+    console.log(users, toDos);
+}
+
 app.post('/add-to-do', (req, res, next) => {
+    log();
     const { id, time, task, completed, user } = req.body.item;
     if (toDos.length > 0) {
         toDos[users.indexOf(user)].push({ id, time, task, completed });
-    } else {
-        toDos.push([{ id, time, task, completed }]);
     }
     res.status(200).json({ toDos: req.body.item });
 });
 
 app.get('/get-to-do/:user', (req, res, next) => {
+    log();
     const { user } = req.params;
     res.status(200).json({ toDos: toDos[users.indexOf(user)] });
 });
 
 app.delete('/del-to-do/:id/:user', (req, res, next) => {
+    log();
     const { id, user } = req.params;
     toDos[users.indexOf(user)] = toDos[users.indexOf(user)].filter(toDo => toDo.id != id);
     res.status(200).json({ toDos: toDos[users.indexOf(user)] });
 });
 
 app.post('/create-to-do-user/:user', (req, res, next) => {
+    log();
     const { user } = req.params;
     const exist = users.find(u => u == user);
     if (!exist) {
         users.push(user);
+        toDos.push([]);
         res.status(200).json({ success: true });
     } else {
         res.status(201).json({ success: false });
@@ -55,11 +62,23 @@ app.post('/create-to-do-user/:user', (req, res, next) => {
 });
 
 app.put('/toggle-to-do/:id/:userName', (req, res, next) => {
+    log();
     const { id, userName } = req.params;
     toDos[users.indexOf(userName)] = toDos[users.indexOf(userName)].map(task => (
         task.id == id ? { ...task, completed: !task.completed } : task
     ));
     res.status(200).json({ success: true });
+});
+
+app.get('/get-to-do-user/:user', (req, res, next) => {
+    log();
+    const { user } = req.params;
+    const exist = users.find(u => u == user);
+    if(exist){
+        return res.status(200).json({ success: true });
+    }else{
+        return res.status(201).json({ success: false });
+    }
 });
 
 const PORT = 3000;
